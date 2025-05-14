5fee581468253637b22ec39781ddfbe9c7bc1ebb
vashud112223 <ashutosh.btech.ec19@iiitranchi.ac.in>
2025-05-14 09:49:24 +0530

    first ep

--- Ep-01/App.js
+++ Ep-01/App.js
@@ -1,18 +1,27 @@
 /** <div id="parent">
   <div id="child">
     <h1>"I'm h1 tag"</h1>
+    <h2>"I'm h2 tag"</h2>
+  </div>
+   <div id="child2">
+    <h1>"I'm h1 tag"</h1>
+    <h2>"I'm h2 tag"</h2>
   </div>
 </div> */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I' h1 tag")
-  )
-);
+// ReactElement(object)=>HTML(Browser understand)
+
+// React is a library beacause it can work on small portion of javascript code or app
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
 
 const heading = React.createElement(
   "h1",
@@ -22,5 +31,5 @@ const heading = React.createElement(
 console.log(heading); //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading); // render take the heading tag put it on the DOM and converted into the h1 tag
-console.log(parent); 
+console.log(parent);
 root.render(parent);
