465cfa10e5325f3c75550db94a742e86c7f1a7f8
Ashutosh Verma <ashutosh.btech.ec19@iiitranchi.ac.in>
2025-05-18 00:46:19 +0530

    sixth episode

--- Ep-04/src/components/RestaurantCard.js
+++ Ep-04/src/components/RestaurantCard.js
@@ -1,29 +1,20 @@
 import { CDN_URL } from "../utils/constants";
 const RestaurantCard = (props) => {
   const { resData } = props;
-  const {
-    cloudinaryImageId,
-    name,
-    totalRatingsString,
-    costForTwo,
-    deliveryTime,
-    cuisines,
-  } = resData?.data;
+  const { cloudinaryImageId, name, avgRating, costForTwo, sla, cuisines } =
+    resData?.info;
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img
         className="res-logo"
         alt="res-logo"
-        src={
-          CDN_URL +
-          cloudinaryImageId
-        }
+        src={CDN_URL + cloudinaryImageId}
       />
       <h3>{name}</h3>
       <h4>{cuisines.join(",")}</h4>
-      <h4>{totalRatingsString} stars</h4>
-      <h4>₹ {costForTwo / 100} for Two </h4>
-      <h4>{deliveryTime}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla?.slaString}</h4>
     </div>
   );
 };
